d0b12ebc1134ce0944466a7b59d3f35aaa282b12
Luong Xuan Trung Dung <luongxuantrungdung@gmail.com>
2025-10-07 17:27:12 +0700

    Setup: packages

---
+++ postcss.config.mjs
@@ -0,0 +1,9 @@
+const config = {
+  darkMode: "class",
+  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
+  theme: {},
+  variants: {},
+  plugins: ["@tailwindcss/postcss"],
+};
+
+export default config;
